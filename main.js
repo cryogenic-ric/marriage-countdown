70771cdbb9a957891818a8f53d6b5bbc91bee56e
Rishi Ranjan <rishi@scrollstack.com>
2021-10-13 19:54:18 +0530

    init

---
+++ main.js
@@ -0,0 +1,164 @@
+import { set } from 'date-fns'
+
+
+
+const days = window.document.querySelector('#days')
+const hours = window.document.querySelector('#hours')
+const minutes = window.document.querySelector('#minutes')
+const seconds = window.document.querySelector('#seconds')
+
+function timeDiffCalc(dateFuture, dateNow) {
+  let diffInMilliSeconds = Math.abs(dateFuture - dateNow) / 1000;
+
+  // calculate days
+  const days = Math.floor(diffInMilliSeconds / 86400);
+  diffInMilliSeconds -= days * 86400;
+
+  // calculate hours
+  const hours = Math.floor(diffInMilliSeconds / 3600) % 24;
+  diffInMilliSeconds -= hours * 3600;
+
+  // calculate minutes
+  const minutes = Math.floor(diffInMilliSeconds / 60) % 60;
+  diffInMilliSeconds -= minutes * 60;
+
+  // calculate minutes
+  const seconds = Math.floor(diffInMilliSeconds) 
+
+  return {
+    days,
+    hours,
+    minutes,
+    seconds
+
+  };
+}
+
+
+const setCounter = (diff) =>{
+  days.innerText= diff.days
+  hours.innerText= diff.hours
+  minutes.innerText= diff.minutes
+  seconds.innerText= diff.seconds
+}
+
+
+
+
+setInterval( () => 
+  setCounter(timeDiffCalc(new Date('2021/12/1 04:10:00'), new Date())), 1000
+)
+
+
+
+
+window.particlesJS("particles", {
+  "particles": {
+    "number": {
+      "value": 100,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#000"
+    },
+    "shape": {
+      "type": "image",
+      "stroke": {
+        "width": 10,
+        "color": "#fff"
+      },
+      "polygon": {
+        "nb_sides": 6
+      },
+      "image": {
+        "src": "https://www.freepnglogos.com/uploads/heart-png/emoji-heart-33.png",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.1,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 5,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 2,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": false,
+      "distance": 50,
+      "color": "#ffffff",
+      "opacity": 0.6,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 5,
+      "direction": "bottom",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": true,
+        "rotateX": 300,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode":  "bubble"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 150,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 200,
+        "size": 10,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.2
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
